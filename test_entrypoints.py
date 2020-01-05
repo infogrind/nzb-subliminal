1bee9839e2454e274f62ba02224cc29a184cac05
Marius Kleiner <kleiner@gmail.com>
2020-01-05 15:49:28 +0100

    Simplify test script

--- test_entrypoints.py
+++ test_entrypoints.py
@@ -2,72 +2,13 @@
 
 import os
 import sys
-import getopt
-import pkg_resources
-
-# Global options with default values.
-verbose = False
-output = "-"
-
 
 def main():
-    args = parse_options(sys.argv[1:])
 
     # Make modules in subdirectory importable
     sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), 'Subliminal'))
     import subliminal
-
-    print "List of defined entry points:"
-    for provider_entry_point in pkg_resources.iter_entry_points(subliminal.PROVIDERS_ENTRY_POINT):
-        print provider_entry_point.name
-
-
-def usage():
-    print("""Usage: <syntax goes here>
-
-    <description goes here>
-
-    Options:
-    -h              Show this help text.
-    -v              Display verbose output.
-    -o <file> Write output to <file> or to stdout if <file> is '-'.
-    """)
-
-
-def parse_options(args):
-
-    # Access global variables
-    global verbose, output
-
-    # Parse options using Getopt; display an error and exit if options could not
-    # be parsed.
-    try:
-        opts, args = getopt.getopt(args, "o:hv")
-    except getopt.GetoptError as err:
-        print(str(err))
-        usage()
-        sys.exit(2)
-
-    # Set variables according to options
-    for opt, val in opts:
-        if opt == "-h":
-            usage()
-            sys.exit()
-
-        elif opt == "-v":
-            verbose = True
-
-        elif opt == "-o":
-            output = val
-        else:
-            assert False, "unhandled option"
-
-    return args
-
-
-def debug(s):
-    if verbose:
-        sys.stderr.write(s + "\n")
+    subliminal.print_entry_points()
 
 
 # This is the most important line: it calls the main function if this program is
